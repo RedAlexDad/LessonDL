e6df0501b7b603bbcb4d700ec0d7e538aad9f593
RedAlexDad <boss6852@gmail.com>
2024-02-19 11:13:27 +0300

    Попытка внедрять

---
+++ exercise/cmake-build-debug/CMakeFiles/LessonDL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LessonDL.dir/example_001ru.cpp.o"
+  "CMakeFiles/LessonDL.dir/example_001ru.cpp.o.d"
+  "LessonDL"
+  "LessonDL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LessonDL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

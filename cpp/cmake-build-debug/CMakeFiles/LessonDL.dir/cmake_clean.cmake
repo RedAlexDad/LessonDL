754d54e02fce42c853f141530ef7ba8513b6ebbf
RedAlexDad <boss6852@gmail.com>
2024-03-09 17:13:21 +0300

    error GPU CUDA

---
+++ cpp/cmake-build-debug/CMakeFiles/LessonDL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LessonDL.dir/hello.cpp.o"
+  "CMakeFiles/LessonDL.dir/hello.cpp.o.d"
+  "LessonDL"
+  "LessonDL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LessonDL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

d9eeb4442c2f7008ed5a4f5820f6260da7225bd7
RedAlexDad <boss6852@gmail.com>
2024-04-01 13:01:11 +0300

    Переустановка LibTorch на CPU
    - Успешная компиляция

---
+++ cpp/cmake-build-debug/check_torch/CMakeFiles/check_torch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check_torch.dir/main.cpp.o"
+  "CMakeFiles/check_torch.dir/main.cpp.o.d"
+  "check_torch"
+  "check_torch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/check_torch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

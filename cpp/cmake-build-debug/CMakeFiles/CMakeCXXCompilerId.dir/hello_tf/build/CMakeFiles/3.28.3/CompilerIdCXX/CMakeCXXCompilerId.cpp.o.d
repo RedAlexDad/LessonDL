7ac0ed4e27c1c17c155ff134c777cf2c53d1c91e
Alexey <boss6852@gmail.com>
2024-04-28 10:14:35 +0300

    Пересборка проекта только на Python

--- cpp/cmake-build-debug/CMakeFiles/CMakeCXXCompilerId.dir/hello_tf/build/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CMakeCXXCompilerId.dir/hello_tf/build/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/redalexdad/Документы/GitHub/LessonDL/cpp/hello_tf/build/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
--- cpp/cmake-build-debug/CMakeFiles/hello_tf_hello_tf.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for hello_tf_hello_tf.
-# This may be replaced when dependencies are built.
